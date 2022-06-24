a2de42d36bba522d5862b246dbf9c67f579af5bc
gafriputra <gafri.putra@gmail.com>
2022-06-24 07:31:48 +0700

    add db transaction

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -12,40 +12,43 @@ use App\Http\Requests\API\CheckoutRequest;
 // use App\Models\Product\Product;
 use App\Models\Transaction;
 use App\Models\TransactionDetail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail as FacadesMail;
 
 class CheckoutController extends Controller
 {
-    public function checkout(CheckoutRequest $request)
+    public function checkout(Request $request)
     {
-        // dd($request->body);
         // masukkan semua dari transaction_details
         $data = $request->except('transaction_details');
         $data['uuid'] = 'TRX-' . mt_rand(10000, 99999) . mt_rand(100, 999);
         $data['shipping'] = 0 ;
         $data['status'] = 'order';
 
-        $transaction = Transaction::create($data);
-
-        foreach ($request->transaction_details as $product) {
-            $details[] = new TransactionDetail([
-                'transaction_id' => $transaction->id,
-                'product_id' => $product[0],
-                'quantity' => $product[1]
-            ]);
-            // mengurangi jumlah produk -1
-            // Product::find($product_id)->decrement('quantity');
+        DB::beginTransaction();
+        try {
+            $transaction = Transaction::create($data);
+            foreach ($request->transaction_details as $product) {
+                $details[] = new TransactionDetail([
+                    'transaction_id' => $transaction->id,
+                    'product_id' => $product[0],
+                    'quantity' => $product[1]
+                ]);
+            }
+            // nyimpan relasinya, lalu save langsung banyak
+            $transaction->details()->saveMany($details);
+            $transaction = Transaction::with('details.product.galleries')->where('uuid', $data['uuid'])->first();
+            // Email
+            FacadesMail::to($transaction->email)->send(
+                new TransactionSuccess($transaction)
+            );
+            DB::commit();
+            return ResponseFormatter::success($transaction);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return ResponseFormatter::error($th->getMessage(), 'error', 500);
         }
-        // nyimpan relasinya, lalu save langsung banyak
-        $transaction->details()->saveMany($details);
 
-
-        $transaction = Transaction::with('details.product.galleries')->where('uuid', $data['uuid'])->first();
-        // Email
-        FacadesMail::to($transaction->email)->send(
-            new TransactionSuccess($transaction)
-        );
-
-        return ResponseFormatter::success($transaction);
     }
 }
